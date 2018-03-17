720607f5407856f91fc5542c1c216b5d2be4b63e
vasirepr <prasanth.v11@gmail.com>
2018-03-17 11:17:41 +0530

    mvn project

---
+++ src/main/java/com/myapp/app/App.java
@@ -0,0 +1,13 @@
+package com.myapp.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
